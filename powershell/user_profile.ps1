33b2618b73bfe727f72b4b9ab60e86ac6db615fe
balits <97095806+balits@users.noreply.github.com>
2022-01-10 17:39:27 +0100

    cool

---
+++ powershell/user_profile.ps1
@@ -0,0 +1,29 @@
+# Import
+Import-Module -Name Terminal-Icons
+Import-Module posh-git
+Import-Module oh-my-posh
+Import-Module PSFzf
+
+# Load Custom Prompt
+function Get-ScriptDirectory {
+    Split-Path $MyInvocation.ScriptName
+}
+$PROMPT_CONFIG = Join-Path (Get-ScriptDirectory) 'pet.omp.json'
+oh-my-posh --init --shell pwsh --config $PROMPT_CONFIG | Invoke-Expression
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+
+# Fzf
+# fzf Set-PSFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl-r'
+
+# Alias 
+Set-Alias .. cd..
+Set-Alias vim nvim
+Set-Alias rn Rename-Item
+Set-Alias im Import-Module
+Set-Alias ll ls
