1e24df241afe2f0ed7626ac5b0ef08cf227dfd98
Balits Peter <97095806+balits@users.noreply.github.com>
2022-01-19 18:44:28 +0100

    scoop is again install only ot HOME\scoop

--- src/practice.ps1
+++ src/practice.ps1
@@ -1,9 +1,10 @@
-$x = Join-Path -Path $env:USERPROFILE -ChildPath '.config\scoop'
+# use this formula to install scoop to your preffered location (maybe even inside .config repo)
+# $x = Join-Path -Path $env:USERPROFILE -ChildPath '.config\scoop'
 # Write-Host $x -ForegroundColor Green
 
 # 1
-[environment]::setEnvironmentVariable('SCOOP', $x, 'User') 
+[environment]::setEnvironmentVariable('SCOOP', $env:USERPROFILE, 'User') 
 # 2
-$env:SCOOP = $x
+$env:SCOOP = $env:USERPROFILE
 # 3
 Invoke-Expression (new-object net.webclient).downloadstring('https://get.scoop.sh') 
